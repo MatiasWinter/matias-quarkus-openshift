225cac595bcbde3b7cd1a597c90a2ace7a78d5a7
Matias Franco <matiaswinter014@gmail.com>
2026-01-28 09:37:13 -0300

    Fix errors in openshift

--- src/main/java/contact/entity/ContactEntity.java
+++ src/main/java/contact/entity/ContactEntity.java
@@ -2,6 +2,7 @@ package contact.entity;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -13,7 +14,7 @@ import lombok.NoArgsConstructor;
 public class ContactEntity {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     public Long id;
     public String name;
     public String email;
